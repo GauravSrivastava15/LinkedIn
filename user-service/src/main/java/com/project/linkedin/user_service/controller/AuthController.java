5165e69b12887055673edf8ccca1abcd4ee263e3
Gaurav.S.Srivastava <gauravsrivastava1501@gmail.com>
2026-02-08 19:24:45 +0530

    User singup and login feature implemented in User service

---
+++ user-service/src/main/java/com/project/linkedin/user_service/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.project.linkedin.user_service.controller;
+
+import com.project.linkedin.user_service.dto.LoginRequestDto;
+import com.project.linkedin.user_service.dto.SignupRequestDto;
+import com.project.linkedin.user_service.dto.UserDto;
+import com.project.linkedin.user_service.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<UserDto> signup(@RequestBody SignupRequestDto signupRequestDto){
+        UserDto userDto = authService.signUp(signupRequestDto);
+        return new ResponseEntity<>(userDto, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequestDto loginRequestDto){
+        String token = authService.login(loginRequestDto);
+        return ResponseEntity.ok(token);
+    }
+}
